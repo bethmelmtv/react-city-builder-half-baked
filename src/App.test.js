f938ff60d7f04a010b4d3a6cf456c9d86dea2382
Beth <bethmelmtv@gmail.com>
2022-05-06 11:46:57 -0700

    worked on fixing bug

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Welcome');
+  const linkElement = screen.getByText('Welcome to beautiful');
   expect(linkElement).toBeInTheDocument();
 });
